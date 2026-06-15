f5c5bacc36ca1d2887b1b3708666c4a85567c9b9
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-15 22:29:12 +0100

    Adição da  verificação de email duplicado no registo

--- backend/registro.php
+++ backend/registro.php
@@ -6,3 +6,12 @@ $password = $_POST['password'];
 $tipo_doc = $_POST['tipo_doc'];
 $num_doc = $_POST['num_doc'];
 $nif = $_POST['nif'];
+
+$stmt = mysqli_prepare($ligacao, "SELECT id FROM atleta WHERE email = ?");
+mysqli_stmt_bind_param($stmt, "s", $email);
+mysqli_stmt_execute($stmt);
+$resultado = mysqli_stmt_get_result($stmt);
+
+if (mysqli_fetch_assoc($resultado)) {
+    die('Este email já está registado.');
+}
