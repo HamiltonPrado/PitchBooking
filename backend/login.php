561929c39dacff47587ffdd1f37686b6932b6e6a
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-15 23:55:18 +0100

    Adição da verificação de password e criação de sessão no login

--- backend/login.php
+++ backend/login.php
@@ -10,3 +10,13 @@ mysqli_stmt_bind_param($stmt, "s", $email);
 mysqli_stmt_execute($stmt);
 $resultado = mysqli_stmt_get_result($stmt);
 $atleta = mysqli_fetch_assoc($resultado);
+
+//verificar a password e criar a sessão//
+if ($atleta && password_verify($password, $atleta['password'])) {
+    $_SESSION['atleta_id'] = $atleta['id'];
+    $_SESSION['nome'] = $atleta['nome'];
+    $_SESSION['role'] = $atleta['role'];
+    echo 'Login com sucesso!';
+} else {
+    echo 'Email ou password incorretos.';
+}
