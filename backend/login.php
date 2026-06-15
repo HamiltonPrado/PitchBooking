56d94f461366732a40722e8c43bb84ede883c652
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-15 23:44:27 +0100

    Implementação da pesquisa do atleta por email na hora do login

--- backend/login.php
+++ backend/login.php
@@ -4,3 +4,9 @@ require 'db.php';
 
 $email = $_POST['email'];
 $password = $_POST['password'];
+
+$stmt = mysqli_prepare($ligacao, "SELECT id, nome, password, role FROM atleta WHERE email = ?");
+mysqli_stmt_bind_param($stmt, "s", $email);
+mysqli_stmt_execute($stmt);
+$resultado = mysqli_stmt_get_result($stmt);
+$atleta = mysqli_fetch_assoc($resultado);
