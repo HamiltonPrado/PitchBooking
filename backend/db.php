941cbb7d961755a842520b0f9f25fe7521c0038a
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-15 21:49:20 +0100

    verificação de erro e charset na ligação

--- backend/db.php
+++ backend/db.php
@@ -6,3 +6,8 @@ $dbname = 'PitchBooking';
 $port = 3306;
 
 $ligacao = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+if (!$ligacao) {
+    die('Erro de ligação a base de dados: ' . mysqli_connect_error());
+}
+mysqli_set_charset($ligacao, 'utf8mb4');
