8e06758536ec56477a0a892f3ac6464540df08a5
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-15 22:10:04 +0100

    introdução dos dados para a recepção dos dados do formulário

--- backend/registro.php
+++ backend/registro.php
@@ -1,2 +1,8 @@
 <?php
 require 'db.php';
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$tipo_doc = $_POST['tipo_doc'];
+$num_doc = $_POST['num_doc'];
+$nif = $_POST['nif'];
