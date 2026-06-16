167b1d3d1151beba1bb6605c6fca9edbf836f10e
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-16 03:16:23 +0100

    registro do pagamento na base de dados

--- backend/pagamento.php
+++ backend/pagamento.php
@@ -15,3 +15,11 @@ $reserva_id = $_POST['reserva_id'];
 $montante = $_POST['montante'];
 $tipo = $_POST['tipo'];
 $operador_id = $_SESSION['atleta_id'];   
+
+// Regista o pagamento na base de dados
+$stmt = mysqli_prepare($ligacao,
+    "INSERT INTO pagamento (reserva_id, montante, tipo, operador_id) VALUES (?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt, "idsi", $reserva_id, $montante, $tipo, $operador_id);
+mysqli_stmt_execute($stmt);
+
+echo 'Pagamento registado com sucesso!';
