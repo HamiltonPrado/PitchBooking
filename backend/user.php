e4bbc010df89029f9c45c3cce807cbdb1300a386
HamiltonPrado <francisco34baptista@gmail.com>
2026-06-16 02:27:55 +0100

    verificação do estado dos documentos do atleta

--- backend/user.php
+++ backend/user.php
@@ -13,4 +13,11 @@ $stmt = mysqli_prepare($ligacao,
 mysqli_stmt_bind_param($stmt, "i", $atleta_id);
 mysqli_stmt_execute($stmt);
 $resultado = mysqli_stmt_get_result($stmt);
-$atleta = mysqli_fetch_assoc($resultado);
+$atleta = mysqli_fetch_assoc($resultado);~
+
+// Verifica se os documentos foram validados pelo backoffice
+if ($atleta['docs_verificados'] == 0) {
+    echo 'Os seus documentos ainda não foram verificados.';
+} else {
+    echo 'Documentos verificados.';
+}
